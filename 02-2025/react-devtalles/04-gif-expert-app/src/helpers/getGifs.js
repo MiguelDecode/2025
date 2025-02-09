b14d103cc5c4b2df13c5b5e59c1cb8d5c4fcab08
Miguel Decode <migueldecode@gmail.com>
2025-02-09 17:07:03 +0100

    feat: add env variables

--- 02-2025/react-devtalles/04-gif-expert-app/src/helpers/getGifs.js
+++ 02-2025/react-devtalles/04-gif-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const apiKey = "";
+  const apiKey = import.meta.env.VITE_API_KEY;
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
 
   const resp = await fetch(url);
